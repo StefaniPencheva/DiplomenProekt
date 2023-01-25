51c609b14a7125ab6f220b522cdad31565986dc6
StefaniPencheva <s.pencheva2018@pmg-pernik.com>
2023-01-25 10:10:59 +0200

    DiplomenProekt

---
+++ WebSiteFurniture/WebSiteFurniture/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebSiteFurniture.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
