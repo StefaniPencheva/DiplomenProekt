22b989c8b719feb707e78505b91d077472e95a5c
StefaniPencheva <s.pencheva2018@pmg-pernik.com>
2023-03-06 00:06:27 +0200

    Home Page 2

--- WebSiteFurniture/WebSiteFurniture/Controllers/HomeController.cs
+++ WebSiteFurniture/WebSiteFurniture/Controllers/HomeController.cs
@@ -31,6 +31,10 @@ namespace WebSiteFurniture.Controllers
         {
             return View();
         }
+        public IActionResult LoocAll()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
